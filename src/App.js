af0c50794c61387f1b94690b98b2c3b43910a5bb
Ahmaaadm <ahmadmoussas8p@gmail.com>
2023-11-27 13:53:04 +0200

    react-weather-app

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import react, {useState} from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=4c2da7d4e0b3ad94568751b0997ab8ae`
+  const searchLocation = (event) => {
+    if(event.key === 'Enter'){
+      axios.get(url)
+    .then((response)=>{
+      setData(response.data)
+      console.log(response.data)
+    }).catch((err)=>{
+      console.log(Error)
+    })
+    setLocation('')
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+        value={location}
+        onChange={event=>setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text" />
+      </div>
+      {data.name &&
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main?<h1>{data.main.temp}F</h1>:null}
+          </div>
+          <div className="description">
+            {data.weather? <p>{data.weather[0].main}</p>:null}
+          </div>
+        </div>
+        
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+      </div>
+}
+    </div>
+  );
+}
+
+export default App;
